f7b459d4fd6075d4871941f4799c4ea46252b335
yuxin2018 <yuxin2018@gmail.com>
2014-03-09 00:02:24 -0800

    update

---
+++ Cookbook/Categories/UILabel+ContentSize.m
@@ -0,0 +1,18 @@
+#import "UILabel+ContentSize.h"
+ 
+@implementation UILabel (ContentSize)
+
+- (CGSize)contentSize {
+   NSMutableParagraphStyle * paragraphStyle = [[NSMutableParagraphStyle alloc] init];
+    paragraphStyle.lineBreakMode = self.lineBreakMode;
+    paragraphStyle.alignment = self.textAlignment;
+    
+    NSDictionary * attributes = @{NSFontAttributeName : self.font,
+                                  NSParagraphStyleAttributeName : paragraphStyle};
+    
+    CGSize contentSize = [self.text boundingRectWithSize:self.frame.size
+                                              options:(NSStringDrawingUsesLineFragmentOrigin|NSStringDrawingUsesFontLeading)
+                                           attributes:attributes
+                                              context:nil].size;
+    return contentSize;
+}
